6e11fdea644dcbe30d7036526fc142dd7067cf41
abdoo13 <45192214+abdoo13@users.noreply.github.com>
2024-03-30 09:11:11 +0100

    Update dsmp_2023_webbapp.py

--- dsmp_2023_webbapp.py
+++ dsmp_2023_webbapp.py
@@ -2,22 +2,24 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+"""from sklearn.preprocessing import LabelEncoder""""
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-data = pd.read_csv('df_.csv')
-# Get column names
-column_names = list(data.columns)[1:-1]
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-st.sidebar.markdown('<h2 style="color: blue;"> Select the values of input variables to predict the electricity prices</h2>', unsafe_allow_html=True)
-user_input_prediction = {}
-for column in column_names:
-  if data[column].dtype != 'O':
-    user_input_prediction[column] = st.sidebar.slider(f'Select {column}', float(data[column].min()), float(data[column].max()), float(data[column].mean()))
-
-st.sidebar.button("Predict Electricity Prices")      
-""""""""""""""""""""""""""""""""""""""""""""""""
+""" """
 st.title('Predicting Electricity Prices at the Alberta Region')
 st.markdown('This app allows predicting Electricty prices for the Alberta region considering the "Residential", "Commercial" and the "Industrial" Sectors.')
+""" """
+path_files = "/content/drive/MyDrive/2023_DSMP_Project/WebApp - StreamLit"
+data = pd.read_csv(path_files + '/' + 'df_.csv')
+# Get column names
+column_names = list(data.columns)[1:-1]
 
+st.sidebar.markdown('<h2 style="color: blue;"> Select the values of input variables to predict the target variable</h2>', unsafe_allow_html=True)
+user_input_prediction = {}
+for column in column_names:
+  #print(column)
+  if data[column].dtype != 'O':
+    user_input_prediction[column] = st.sidebar.slider(f'{column}', float(data[column].min()), float(data[column].max()), float(data[column].mean()))
+""" """
 df = pd.DataFrame()
 list_ = sorted(data['Sector'].unique().tolist())
 #
@@ -26,31 +28,31 @@ for sector_ in list_:
   user_input_prediction['Sector'] = list_.index(sector_)
   df = pd.concat([df, pd.DataFrame([user_input_prediction])], axis = 0, ignore_index=True)
 df['Sector'] = df['Sector'].astype('float')
-
-# Load the ML Model
+""" """
 model = joblib.load('lgbm_model.sav')
 model.set_params(n_classes=1)
 
-# Predict and display the results
-#prediction = model.predict(temp.values.reshape(1, -1))
+""" """
 preds = model.predict(df.values)
 st.subheader('Prediction')
-st.write(f'The predicted Electricity Price is: {np.round(preds,2)}')
-
-# Predict Button
-st.button("Predict Electricity Prices")
 #
-if st.button("Predict Electricity Prices"):
-  result = model.predict(df.values)
-  st.text(np.round(preds,2))
-
-# Generate Plot
-df_output = pd.DataFrame(np.round(preds,2)).T
-df_output.columns = list_
-#df_output
-#
-#
-
+if st.sidebar.button("Predict Electricity Prices"):
+  dict_ = {}
+  for column in column_names:
+    if data[column].dtype != 'O':
+      dict_[column] = user_input_prediction[column]
+    else:
+      dict_[column] = list(data['Sector'].unique())
+  df_pred = pd.DataFrame(dict_)
+  le = LabelEncoder()
+  df_pred['Sector'] = le.fit_transform(df_pred['Sector'])
+  features_list = df_pred.values.tolist()
+  preds = model.predict(features_list)
+  """ """
+  df_output = pd.DataFrame(np.round(preds,2)).T
+  df_output.columns = list_
+  st.text(df_output)
+  st.bar_chart(df_output)
 """fig, ax=plt.subplots(figsize=(8,5))
 colors_ = sns.color_palette("deep")
 ax = sns.barplot(df_output, palette=colors_)
@@ -61,5 +63,3 @@ plt.ylabel('Electricity Price - (CAD Cents/KWh)')
 plt.show()
 st.pyplot(fig)
 """
-
-st.bar_chart(df_output)
